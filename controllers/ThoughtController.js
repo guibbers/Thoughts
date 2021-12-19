c0167a7b08df2d09de35ec673ded24af7095c641
Guilherme Torres <53585353+guibbers@users.noreply.github.com>
2021-12-18 22:14:25 -0300

    finish lesson

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -22,7 +22,13 @@ module.exports = class ThoughtController {
 
     const thoughts = user.Thoughts.map((result) => result.dataValues)
 
-    res.render('thoughts/dashboard', { thoughts })
+    let emptyThoughts = false
+
+    if (thoughts.length === 0) {
+      emptyThoughts = true
+    }
+
+    res.render('thoughts/dashboard', { thoughts, emptyThoughts })
   }
 
   static async createThought(req, res) {
